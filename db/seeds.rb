189b385b4700b975cd8a36752198207f9e8f46d8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-01 16:34:57 +0000

    Half-way though db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+q1 = Quiz.new(question: 'What colour is the sky?', answer1: 'Blue', answer2: 'Red', answer3: 'Green', answer4: 'Yellow', correct_answer: 'Blue')
