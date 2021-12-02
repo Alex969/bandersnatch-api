ff48cebececb8fa743a0efb92ad0d331630eda91
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-02 15:06:51 +0000

    Creating and updating a user tested in terminal http requests

--- scratch.rb
+++ scratch.rb
@@ -1,1 +1,6 @@
-rails g scaffold quiz question:string answer1:string answer2:string answer3:string answer4:string correct_answer:string
+To create a user:
+curl -d "user[name]=Jane" -d "user[score]=0" -d "user[questions_answered]=0" https://bandersnatch-api.herokuapp.com/users
+
+
+To update a person's scores:
+curl -X PUT -d "user[score]=1" -d "user[questions_answered]=2" https://bandersnatch-api.herokuapp.com/users/3 
