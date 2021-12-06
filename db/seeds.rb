4270c7bdc0bfb42288ae0835d08c3f0ad2fe837f
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-06 14:56:38 +0000

    Changing db structure, q1 always correct answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Quiz.create(question: 'What colour is the sky?', answer1: 'Blue', answer2: 'Red', answer3: 'Green', answer4: 'Yellow', correct_answer: 'Blue')
-Quiz.create(question: 'What planet are we on?', answer1: 'Mars', answer2: 'Earth', answer3: 'Mercury', answer4: 'Neptune', correct_answer: 'Earth')
-Quiz.create(question: 'What is the capital of France?', answer1: 'London', answer2: 'Paris', answer3: 'Moscow', answer4: 'Washington', correct_answer: 'Paris')
-Quiz.create(question: 'What is 2 + 2?', answer1: '1', answer2: '5', answer3: '8', answer4: '4', correct_answer: '4')
-Quiz.create(question: 'What is the fastest land mammal?', answer1: 'Dog', answer2: 'Wolf', answer3: 'Hippo', answer4: 'Cheetah', correct_answer: 'Cheetah')
+Quiz.create(question: 'What planet are we on?', answer1: 'Earth', answer2: 'Mars', answer3: 'Mercury', answer4: 'Neptune', correct_answer: 'Earth')
+Quiz.create(question: 'What is the capital of France?', answer1: 'Paris', answer2: 'London', answer3: 'Moscow', answer4: 'Washington', correct_answer: 'Paris')
+Quiz.create(question: 'What is 2 + 2?', answer1: '4', answer2: '5', answer3: '8', answer4: '1', correct_answer: '4')
+Quiz.create(question: 'What is the fastest land mammal?', answer1: 'Cheetah', answer2: 'Wolf', answer3: 'Hippo', answer4: 'Dog', correct_answer: 'Cheetah')
 
 User.create(name: 'Alan', score: 3, questions_answered: 3)
 # cosmetic change
